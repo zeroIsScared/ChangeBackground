f4bc81882262fecace4639ab4e7f9b5a43c263d1
zeroIsScared <69678222+zeroIsScared@users.noreply.github.com>
2023-05-23 09:31:51 +0300

    Update change-background.js

--- change-background.js
+++ change-background.js
@@ -1,9 +1,5 @@
 const myButton= document.querySelector('button');
-
-myButton.style.cssText +='display:flex;justify-content:center';
 const page=document.body;
-
-
 
 const changeBackgrond = function(){
     const colors= ['yellow','red','green','blue','black','orange','pink'];
