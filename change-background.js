2cba47ee337471b8c9aec181213dc9dc12f87f29
zeroIsScared <69678222+zeroIsScared@users.noreply.github.com>
2023-05-25 11:16:43 +0300

    Update change-background.js

--- change-background.js
+++ change-background.js
@@ -1,10 +1,12 @@
-const myButton= document.querySelector('button');
-const page=document.body;
+const myButton = document.querySelector('button');
+const page = document.body;
 
-const changeBackgrond = function(){
-    const colors= ['yellow','red','green','blue','black','orange','pink'];
-    const randomColor= colors[Math.floor(Math.random()*colors.length)];   
-    page.style.backgroundColor= randomColor;
+const randomNumber = (num) => Math.floor(Math.random() * num);
+
+const changeBackgrond = function () {
+    const colors = ['yellow', 'red', 'green', 'blue', 'black', 'orange', 'pink'];
+    const randomColor = colors[randomNumber(colors.length)];
+    page.style.backgroundColor = randomColor;
 }
 
 myButton.addEventListener('click', changeBackgrond);
